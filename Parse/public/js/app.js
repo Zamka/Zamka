f530edc3822da2456dacce9ee31ba8b1c94f0301
gogodr <gogodr@hotmail.com>
2015-06-25 10:51:32 -0500

    Plataforma Usuario 1

--- Parse/public/js/app.js
+++ Parse/public/js/app.js
@@ -1,21 +1,41 @@
 angular.module('ZamkaAdmin', ['ngMaterial','ngRoute'])
-.config(function($mdThemingProvider,$routeProvider) {
+.config(function($mdThemingProvider,$routeProvider,$interpolateProvider) {
   $routeProvider
-	.when('/', {
-    templateUrl: '/partials/app/index.html',
-    controller: 'indexCtrl'
-  	});
+      .when('/', {
+      templateUrl: '/partials/app/index.html',
+      controller: 'indexCtrl'
+      })
+      .when('/Login', {
+        templateUrl: '/partials/app/login.html',
+        controller: 'indexCtrl'
+      })
+      .when('/Eventos', {
+        templateUrl: '/partials/app/eventos.html',
+        controller: 'indexCtrl'
+      })
+      .when('/Evento/:id', {
+        templateUrl: '/partials/app/evento.html',
+        controller: 'indexCtrl'
+      })
+      .when('/Perfil/:id', {
+        templateUrl: '/partials/app/perfil.html',
+        controller: 'indexCtrl'
+      })
+      .when('/ONG/:id', {
+        templateUrl: '/partials/app/ong.html',
+        controller: 'indexCtrl'
+      });
   $mdThemingProvider.theme('default')
     .primaryPalette('light-blue')
     .accentPalette('grey');
+  $interpolateProvider.startSymbol('[[');
+  $interpolateProvider.endSymbol(']]');
 })
-.controller('AppCtrl', ['$scope', '$mdSidenav', function($scope, $mdSidenav){
+.controller('AppCtrl', function($scope, $mdSidenav){
   $scope.toggleSidenav = function(menuId) {
     $mdSidenav(menuId).toggle();
   };
-}])
-.controller('indexCtrl', ['$scope', '$mdSidenav', function($scope, $mdSidenav){
-  $scope.toggleSidenav = function(menuId) {
-    $mdSidenav(menuId).toggle();
-  };
-}]);
+})
+.controller('indexCtrl',function($scope){
+
+});
