c588524d06d886f990fa5e853bed144a72097293
Ricardo <pst.falco@gmail.com>
2015-08-10 17:59:00 -0500

    Participacion

--- Parse/cloud/main.js
+++ Parse/cloud/main.js
@@ -1,51 +1,23 @@
 require('cloud/app.js');
 
-Parse.Cloud.define("Login", function (request, response) {
+var Participacion = Parse.Object.extend("Participacion");
 
-
-
-    response.success("login");
-});
-
-Parse.Cloud.define("Registrar", function (request, response) {
-
-    var email = request.params.email;
-    var password = request.params.password;
-    var cover = new Parse.File("cover.png", request.params.cover);
-    var foto = new Parse.File("photo.png", request.params.photo);
-    var name = request.params.name;
-    var gustos = request.params.gustos;
-    var bio = request.params.bio;
-    var gender = request.params.gender;
-
-    //Parse.Cloud.useMasterKey();
-
-    var user = new Parse.User();
-    user.setUsername(email);
-    user.setEmail(email);
-    user.setPassword(password);
-
-    //user.set("cover", cover);
-    //user.set("foto", foto);
-
-    user.set("name", name);
-    user.set("bio", bio);
-    user.set("gender", gender);
-
-    cover.save().then(function () {
-        return foto.save();
-    }, function (error) {
-        response.error(error);
-    }).then(function () {
-
-    }, function (error) {
-        response.error(error);
-    }).then(function () {
-
-    }, function (error) {
-        response.error(error);
+Parse.Cloud.beforeSave("Participacion", function (request, response) {
+    var evento = request.object.get("Evento");
+    var usuario = request.object.get("Usuario");
+    var query = new Parse.Query(Participacion);
+    query.equalTo("Usuario", usuario);
+    query.equalTo("Evento", evento);
+    query.count({
+        success: function (count) {
+            if (count === 0) {
+                response.success();
+            } else {
+                response.error("Usuario Ya inscrito");
+            }
+        },
+        error: function (error) {
+            response.error(error);
+        }
     });
-
-    response.success(user);
-
 });
